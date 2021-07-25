ee5399462eabace9164b955c61f7df423cec7c87
Jing <ltesr125124015@gmail.com>
2021-07-26 00:55:55 +0800

    Default Changelist

--- C++ string/main.cpp
+++ C++ string/main.cpp
@@ -9,6 +9,6 @@ int main() {
     char c1 = s1.at(0); // at(int) 取得字串位「字元」
     char c2 = s2->at(0); //物件「.」呼叫成員函數，指標「->」呼叫成員函數
     cout << c1 << " " << c2 << endl;
-    cout << s1[0] << " " << (*s2)[0] << "\n";
+    cout << s1[0] << " " << (*s2)[0] << endl;
     return 0;
 }
