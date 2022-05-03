81bb3c21dfe6b867d48c4f1f79c3b6df68a0074f
ewq897456321 <ltesr125124015@gmail.com>
2022-05-04 00:32:24 +0800

    add CPE1260

---
+++ CPE1260/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T = 0;
+    scanf("%d", &T);
+    for (int l = 0; l < T; l++) {
+        int num;
+        long long sum = 0;
+        scanf("%d", &num);
+        int *arr = (int *)malloc(sizeof(int));
+        for (int i = 0; i < num; i++) {
+            scanf("%d", &arr[i]);
+            if (i != 0) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (arr[i] >= arr[j])
+                        sum += 1;
+                }
+            }
+        }
+        free(arr);
+        cout << sum << endl;
+    }
+}
