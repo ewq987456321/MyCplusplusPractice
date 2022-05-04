484ad9058a4461c72d1793919d9483089f1e20ef
ewq897456321 <ltesr125124015@gmail.com>
2022-05-04 22:30:50 +0800

    fix answer error 2

--- CPE11078/main.cpp
+++ CPE11078/main.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int k = 0; k < T; k++) {
         int num;
         long long max_amount = -99999999;
-        int A, B, temp = -1;
+        int A, B, temp = -1, plus;
         cin >> num;
         int *arr = (int *)malloc(sizeof(int) * num);
         for (int i = 0; i < num; i++) {
@@ -19,17 +19,22 @@ int main() {
                 B = arr[i];
                 max_amount = A - B;
             }
-            if (temp != -1 && arr[i] < B) {
+            if (temp != -1 && (plus > arr[i] - B)) {
                 A = temp;
-                temp = -1;
+                B = arr[i];
+                max_amount = A - B;
+                // temp = -1;
             }
-            if (arr[i] > A)
+            if (arr[i] > A) {
                 temp = arr[i];
+                plus = temp - A;
+            }
             if (arr[i] < B) {
                 max_amount = A - arr[i];
                 B = arr[i];
             }
-            // cout << max_amount << " " << temp << endl;
+            // cout << max_amount << " " << temp << " " << plus << " " <<
+            // (arr[i] - B) << endl;
         }
         cout << max_amount << endl;
         free(arr);
