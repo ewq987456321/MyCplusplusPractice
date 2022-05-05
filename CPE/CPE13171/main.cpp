c5a215ad2c78f6bfb811fe9da8c35bfdcb1bf692
ewq897456321 <ltesr125124015@gmail.com>
2022-05-05 10:36:46 +0800

    mv each CPE to CPE folder

---
+++ CPE/CPE13171/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    map<char, string> colorPair{{'M', "100"}, {'Y', "001"}, {'C', "010"},
+                                {'R', "101"}, {'B', "111"}, {'G', "011"},
+                                {'V', "110"}, {'W', "000"}};
+    for (int l = 0; l < num; l++) {
+        int M, C, Y;
+        string color, temp;
+        cin >> M >> Y >> C >> color;
+        for (unsigned long long i = 0; i < color.length(); i++) {
+            M -= (colorPair[color[i]][0] - '0');
+            C -= (colorPair[color[i]][1] - '0');
+            Y -= (colorPair[color[i]][2] - '0');
+            // cout << "YES " << M << " " << Y << " " << C << endl;
+        }
+        if (M >= 0 && C >= 0 && Y >= 0)
+            cout << "YES " << M << " " << Y << " " << C << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
